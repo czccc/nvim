9018606c00b321a2238224af7f78409be7cf05c8
Cheng <lichchchn@gmail.com>
2022-04-20 21:08:01 +0800

    feat: add nightfox theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -18,6 +18,14 @@ M.packers = {
       require("plugins.themes").setup_onedark()
     end,
   },
+  {
+    "EdenEast/nightfox.nvim",
+    -- opt = true,
+    config = function()
+      require("plugins.themes").setup_nightfox()
+    end,
+    -- disable = true,
+  },
 }
 
 M.setup_tokyonight = function()
@@ -73,7 +81,9 @@ M.setup_onedark = function()
     },
 
     -- Custom Highlights --
-    colors = {}, -- Override default colors
+    colors = {
+      bg0 = "#282c34",
+    }, -- Override default colors
     highlights = {
       StatusLine = { bg = "$bg1" },
       SignColumn = { bg = "none" },
@@ -112,4 +122,34 @@ M.setup_onedark = function()
   cl.setup_colorscheme()
 end
 
+M.setup_nightfox = function()
+  require("nightfox").setup({
+    options = {
+      -- Compiled file's destination location
+      compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+      compile_file_suffix = "_compiled", -- Compiled file suffix
+      transparent = false, -- Disable setting background
+      terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+      dim_inactive = false, -- Non focused panes set to alternative background
+      styles = { -- Style to be applied to different syntax groups
+
+        comments = "italic",
+        keywords = "bold",
+        types = "italic,bold",
+        functions = "NONE",
+        numbers = "NONE",
+        strings = "NONE",
+        variables = "NONE",
+      },
+      inverse = { -- Inverse highlight for different types
+        match_paren = false,
+        visual = false,
+        search = false,
+      },
+      modules = { -- List of various plugins and additional options
+        -- ...
+      },
+    },
+  })
+end
 return M
