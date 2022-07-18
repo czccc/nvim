1c7624f8bab386b4396e0cd819fa56b083f84047
Cheng <lichchchn@gmail.com>
2022-07-18 21:22:01 +0800

    remove unused colorscheme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,6 +1,5 @@
 local M = {}
 
--- M.colorscheme = "catppuccin"
 M.colorscheme = "onedark"
 
 M.init = function()
@@ -10,63 +9,35 @@ end
 
 M.packers = {
   {
-    "folke/tokyonight.nvim",
-    config = function()
-      require("plugins.themes").setup_tokyonight()
-    end,
-  },
-  {
     "navarasu/onedark.nvim",
     -- opt = true,
     config = function()
       require("plugins.themes").setup_onedark()
     end,
   },
-  {
-    "EdenEast/nightfox.nvim",
-    -- opt = true,
-    config = function()
-      require("plugins.themes").setup_nightfox()
-    end,
-    -- disable = true,
-  },
-  {
-    "catppuccin/nvim",
-    as = "catppuccin",
-    -- opt = true,
-    config = function()
-      require("plugins.themes").setup_catppuccin()
-    end,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   config = function()
+  --     require("plugins.themes").setup_tokyonight()
+  --   end,
+  -- },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   -- opt = true,
+  --   config = function()
+  --     require("plugins.themes").setup_nightfox()
+  --   end,
+  --   -- disable = true,
+  -- },
+  -- {
+  --   "catppuccin/nvim",
+  --   as = "catppuccin",
+  --   -- opt = true,
+  --   config = function()
+  --     require("plugins.themes").setup_catppuccin()
+  --   end,
+  -- },
 }
-
-M.setup_tokyonight = function()
-  vim.g.tokyonight_dev = true
-  vim.g.tokyonight_style = "storm"
-  vim.g.tokyonight_sidebars = {
-    "qf",
-    "vista_kind",
-    "terminal",
-    "packer",
-    "spectre_panel",
-    "NeogitStatus",
-    "help",
-    "Outline",
-  }
-  vim.g.tokyonight_cterm_colors = false
-  vim.g.tokyonight_terminal_colors = true
-  vim.g.tokyonight_italic_comments = true
-  vim.g.tokyonight_italic_keywords = true
-  vim.g.tokyonight_italic_functions = false
-  vim.g.tokyonight_italic_variables = false
-  vim.g.tokyonight_hide_inactive_statusline = true
-  vim.g.tokyonight_dark_sidebar = true
-  vim.g.tokyonight_dark_float = true
-  vim.g.tokyonight_colors = {
-    -- NvimTreeGitDirty = "orange",
-    -- FocusedSymbol = "Visual",
-  }
-end
 
 M.setup_onedark = function()
   -- vim.g.onedark_disable_terminal_colors = true
@@ -147,6 +118,34 @@ M.setup_onedark = function()
   -- require("onedark").load()
   vim.g.colors_name = M.colorscheme
   vim.cmd("colorscheme " .. M.colorscheme)
+end
+
+M.setup_tokyonight = function()
+  vim.g.tokyonight_dev = true
+  vim.g.tokyonight_style = "storm"
+  vim.g.tokyonight_sidebars = {
+    "qf",
+    "vista_kind",
+    "terminal",
+    "packer",
+    "spectre_panel",
+    "NeogitStatus",
+    "help",
+    "Outline",
+  }
+  vim.g.tokyonight_cterm_colors = false
+  vim.g.tokyonight_terminal_colors = true
+  vim.g.tokyonight_italic_comments = true
+  vim.g.tokyonight_italic_keywords = true
+  vim.g.tokyonight_italic_functions = false
+  vim.g.tokyonight_italic_variables = false
+  vim.g.tokyonight_hide_inactive_statusline = true
+  vim.g.tokyonight_dark_sidebar = true
+  vim.g.tokyonight_dark_float = true
+  vim.g.tokyonight_colors = {
+    -- NvimTreeGitDirty = "orange",
+    -- FocusedSymbol = "Visual",
+  }
 end
 
 M.setup_nightfox = function()
@@ -256,8 +255,8 @@ M.setup_catppuccin = function()
       symbols_outline = true,
     },
   })
-  local colors = require("catppuccin.api.colors").get_colors() -- fetch colors with API
-  catppuccin.remap({ Comment = { fg = colors.flamingo } })
+  -- local colors = require("catppuccin.api.colors").get_colors() -- fetch colors with API
+  -- catppuccin.remap({ Comment = { fg = colors.flamingo } })
 end
 
 return M
